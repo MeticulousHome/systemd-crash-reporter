01753029e193b85ab1b2ec894d69be46ff56c82f
Mimoja <git@mimoja.de>
2025-04-22 18:19:30 +0200

    add build script with docker for use in the system image
    Signed-off-by: Mimoja <git@mimoja.de>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DIR=$(dirname $0)
+PROJECT="$(pwd)/${DIR}"
+
+# Run to register qemu static
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+docker run --rm  \
+    -it \
+    --platform=linux/arm64/v8 \
+    -v ${PROJECT}:/app \
+    ${CACHE_OPTIONS} \
+    -w /app \
+    arm64v8/rust \
+    bash -c "apt update && apt -y install libssl-dev && cargo build --release"
