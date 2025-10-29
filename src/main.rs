9c377cac4a53ecbf8a4345fbaad56060be166ebf
Mimoja <git@mimoja.de>
2025-10-29 21:00:04 +0100

    run cargo fmt
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::env;
-use sentry::{protocol::{Attachment, AttachmentType}, types::Dsn, Client, Hub, Scope};
-use std::process::Command;
 use chrono::Utc;
-use std::sync::Arc;
+use sentry::{
+    Client, Hub, Scope,
+    protocol::{Attachment, AttachmentType},
+    types::Dsn,
+};
+use std::env;
 use std::fs;
+use std::process::Command;
+use std::sync::Arc;
 
 fn main() {
     let _guard = sentry::init((
@@ -14,7 +18,7 @@ fn main() {
             ..Default::default()
         },
     ));
-    
+
     let sc_dsn: Dsn = "https://295725e5bbfc9b3eb0413cafc1f6cea6@o4506723336060928.ingest.us.sentry.io/4509197049593856".parse().unwrap();
 
     let mut sc_opts = sentry::ClientOptions {
@@ -26,9 +30,8 @@ fn main() {
     sc_opts.dsn = Some(sc_dsn);
 
     sc_opts.transport = Some(Arc::new(sentry::transports::DefaultTransportFactory));
-    
-    let secondary_client = Some(Arc::new(Client::from_config(sc_opts)));
 
+    let secondary_client = Some(Arc::new(Client::from_config(sc_opts)));
 
     // Extract environment variables provided by systemd
     let unit = env::var("MONITOR_UNIT").unwrap_or_else(|_| "unknown".into());
@@ -47,37 +50,46 @@ fn main() {
         unit, job_result, exit_code, exit_status, invocation_id
     );
 
-    // Get 5 mins worth of logs from the crashed service, buffer it  
+    // Get 5 mins worth of logs from the crashed service, buffer it
     let output = Command::new("sh")
-                                                        .arg("-c")
-                                                        .arg(format!("journalctl --no-pager -u {} --since=\"5 minutes ago\"", unit))
-                                                        .output();
-    const BUILD_VERSION_PATH : &str = "/opt/image-build-version";
-    const BUILD_DATE_PATH : &str = "/opt/ROOTFS_BUILD_DATE";
-    
+        .arg("-c")
+        .arg(format!(
+            "journalctl --no-pager -u {} --since=\"5 minutes ago\"",
+            unit
+        ))
+        .output();
+    const BUILD_VERSION_PATH: &str = "/opt/image-build-version";
+    const BUILD_DATE_PATH: &str = "/opt/ROOTFS_BUILD_DATE";
+
     let build_version = match fs::read_to_string(BUILD_VERSION_PATH) {
-        Ok(result) => {
-            result
-        }
-        Err(cause) =>{
-            println!("Error {} while reading file at: {}", cause, BUILD_VERSION_PATH);
+        Ok(result) => result,
+        Err(cause) => {
+            println!(
+                "Error {} while reading file at: {}",
+                cause, BUILD_VERSION_PATH
+            );
             String::from("unknown")
         }
     };
-    
+
     let build_date = match fs::read_to_string(BUILD_DATE_PATH) {
-        Ok(result) =>{
-            result
-        }
-        
-        Err(cause) =>{
-            println!("Error {} while reading file at: {}", cause, BUILD_VERSION_PATH);
+        Ok(result) => result,
+
+        Err(cause) => {
+            println!(
+                "Error {} while reading file at: {}",
+                cause, BUILD_VERSION_PATH
+            );
             String::from("unknown")
         }
     };
 
     sentry::configure_scope(|scope: &mut Scope| {
-        let header = format!("\n =============== LAST 5 MINUTES OF LOGS FROM {} ===============\n\n", unit.to_ascii_uppercase()).into_bytes();
+        let header = format!(
+            "\n =============== LAST 5 MINUTES OF LOGS FROM {} ===============\n\n",
+            unit.to_ascii_uppercase()
+        )
+        .into_bytes();
         let data: Vec<u8> = match output {
             Ok(cmd_out) => {
                 let logs = String::from_utf8_lossy(&cmd_out.stdout).to_ascii_lowercase();
@@ -98,19 +110,20 @@ fn main() {
 
         let date = Utc::now();
         let filename = format!("{hostname} {unit} {date} crash logs.txt");
-        
+
         let attachment: Attachment = Attachment {
-                                                        buffer: data,
-                                                        filename: filename,
-                                                        content_type: Some("text/plain".to_string()),
-                                                        ty: Some(AttachmentType::Attachment) };
+            buffer: data,
+            filename: filename,
+            content_type: Some("text/plain".to_string()),
+            ty: Some(AttachmentType::Attachment),
+        };
 
         let mut map = std::collections::BTreeMap::new();
         map.insert(String::from("unit"), unit.clone().into());
         map.insert(String::from("hostname"), hostname.clone().into());
         scope.set_context("machine", sentry::protocol::Context::Other(map));
         scope.add_attachment(attachment);
-        scope.set_tag("hostname",hostname.clone());
+        scope.set_tag("hostname", hostname.clone());
         scope.set_tag("build-version", build_version.clone());
         scope.set_tag("build-date", build_date.clone());
     });
@@ -118,7 +131,7 @@ fn main() {
     // Send to Sentry
     sentry::capture_message(&error_msg, sentry::Level::Error);
 
-    Hub::with_active(|hub|{
+    Hub::with_active(|hub| {
         hub.bind_client(secondary_client);
         hub.capture_message(&error_msg, sentry::Level::Error);
     });
